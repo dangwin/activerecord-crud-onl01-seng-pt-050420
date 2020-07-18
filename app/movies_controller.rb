8726a7d0bcb6372ee25d03ca4f24263975ade135
Dang Nguyen <danghwin@gmail.com>
2020-07-18 13:53:45 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,7 +30,7 @@ def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990 }
   # title == "Home Alone"
   # release_date == 1990
   
-  Movie.create do |m|
+  Movie.create do |movie|
       movie.title = args[:title]
     movie.release_date = args[:release_date]
   end
